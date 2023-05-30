87296cb189df74472fc60e49092b8095a9b0feae
baongoc02 <105281228+baongoc02@users.noreply.github.com>
2023-05-31 00:39:22 +0700

    fix X-Content-Type-Options Header Missing

---
+++ PetShop/src/main/java/com/petshop/filters/ContentTypeFilter.java
@@ -0,0 +1,32 @@
+package com.petshop.filters;
+import java.io.IOException;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class ContentTypeFilter implements Filter {
+	@Override
+	public void init(FilterConfig filterconfig) throws ServletException {
+		Filter.super.init(filterconfig);
+	}
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+		httpServletResponse.setHeader("X-Content-Type-Options", "nosniff");
+		
+			httpServletResponse.setHeader("Content-Security-Policy",
+					"script-src 'self' https://cdn.jsdelivr.net https://code.jquery.com https://ajax.googleapis.com ");
+		
+		
+		chain.doFilter(request, response);
+	}
+	@Override
+	public void destroy() {
+		Filter.super.destroy();
+	}
+}
